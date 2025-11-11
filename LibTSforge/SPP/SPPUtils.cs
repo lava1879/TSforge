c18bd98e51610ce56e28fcc6c693b23dba1d46c8
lava1879 <magicv4@proton.me>
2025-11-11 15:23:13 +0100

    actually, it's not the Win7 one

--- LibTSforge/SPP/SPPUtils.cs
+++ LibTSforge/SPP/SPPUtils.cs
@@ -263,7 +263,7 @@ namespace LibTSforge.SPP
             switch (version)
             {
                 case PSVersion.Vista:
-                    return new PhysicalStoreWin7(psPath, production);
+                    return new PhysicalStoreVista(psPath, production);
                 case PSVersion.Win7:
                     return new PhysicalStoreWin7(psPath, production);
                 default:
